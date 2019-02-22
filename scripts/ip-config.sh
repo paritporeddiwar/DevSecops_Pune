e1f11fd00d27bdf6b0d5fc8e4d587d5abd967d8c
paritporeddiwar <parit.28@gmail.com>
2019-02-22 15:09:27 +0530

    modified

--- scripts/ip-config.sh
+++ scripts/ip-config.sh
@@ -1,3 +1,4 @@
-TRAVIS_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gruyere) 
+#TRAVIS_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gruyere) 
 
-echo "default: TARGET_HOSTNAME=$TRAVIS_IP" > ./config/cucumber.yml
+#echo "default: TARGET_HOSTNAME=$TRAVIS_IP" > ./config/cucumber.yml
+echo "default: TARGET_HOSTNAME=https://syncoic.preview.climatixic.com/#/Login" > ./config/cucumber.yml
